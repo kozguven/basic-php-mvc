4dfd7801490471926f11a4e795852b0ea786bbdf
Kerem Ozguven <ozguvenkerem@gmail.com>
2024-06-08 23:40:00 +0300

    fixes

--- public/index.php
+++ public/index.php
@@ -5,17 +5,17 @@ require_once '../app/core/Router.php';
 require_once '../app/core/Controller.php';
 require_once '../app/core/Model.php';
 require_once '../app/core/Validation.php';
-require_once '../app/config/database.php';
+require_once '../app/core/database.php';
 
 session_start();
 
 $router = new Router();
 
 // Add routes
-$router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('register', ['controller' => 'User', 'action' => 'register']);
-$router->add('login', ['controller' => 'User', 'action' => 'login']);
-$router->add('logout', ['controller' => 'User', 'action' => 'logout']);
+$router->add('/', ['controller' => 'Home', 'action' => 'index']);
+$router->add('/register', ['controller' => 'User', 'action' => 'register']);
+$router->add('/login', ['controller' => 'User', 'action' => 'login']);
+$router->add('/logout', ['controller' => 'User', 'action' => 'logout']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 
